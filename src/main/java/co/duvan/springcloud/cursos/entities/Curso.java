ae35633a67c55428aa9da16e974bf85ee1f8c949
DUVAN0829 <duvancasgonz29@gmail.com>
2025-05-18 21:28:25 -0500

    Refactoriza 'application.properties' y 'Curso'

--- src/main/java/co/duvan/springcloud/cursos/entities/Curso.java
+++ src/main/java/co/duvan/springcloud/cursos/entities/Curso.java
@@ -1,15 +1,13 @@
 package co.duvan.springcloud.cursos.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "cursos")
 public class Curso {
 
     //Vars
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
